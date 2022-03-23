8c2d3616935849c8a45c94c529157d8cadbd1bca
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-03-23 18:23:42 +0200

    My commit message

---
+++ models/index.js
@@ -0,0 +1,12 @@
+const employee = require('./employee');
+const project = require('./project');
+const project_pImage = require('./project_pImage');
+const task = require('./task');
+const workpackage = require('./workpackage');
+
+module.exports = {
+  employee,
+  project,
+  project_pImage,
+  task,
+  workpackage};
